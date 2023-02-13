b5ece0a2d0d50c2c61acdf2b1cbe13a2817b8ea5
oibioib <pavelbodal@gmail.com>
2023-02-13 13:15:17 +0100

    feat: add new methods to project controller

--- src/controllers/Project.ts
+++ src/controllers/Project.ts
@@ -3,18 +3,23 @@ import { Equal } from 'typeorm';
 import { validate as uuidValidate } from 'uuid';
 
 import dataSource from '../data-source/data-source';
-import { Project } from '../entity';
+import { Project, User } from '../entity';
 
 const getUserProjects = async (req: Request, res: Response) => {
-  const userId = req.params.uuid;
+  const requestUserId = req.params.uuid;
 
-  if (!uuidValidate(userId)) {
+  if (!requestUserId) {
+    res.status(400).send('No user id');
+    return;
+  }
+
+  if (!uuidValidate(requestUserId)) {
     res.status(400).send('Invalid user id');
     return;
   }
 
   const userProjects = await dataSource.manager.findBy(Project, {
-    user: Equal(userId),
+    user: Equal(requestUserId),
   });
 
   if (userProjects) {
@@ -24,6 +29,53 @@ const getUserProjects = async (req: Request, res: Response) => {
   }
 };
 
+const addUserProject = async (req: Request, res: Response) => {
+  if (!req.body) {
+    res.status(400).send('No request body');
+    return;
+  }
+
+  const requestUserId = req.body.userId;
+  const requestTitle = req.body.title || '';
+
+  if (!requestUserId) {
+    res.status(400).send('No user id');
+    return;
+  }
+
+  const user = await dataSource.manager.findOneBy(User, { id: requestUserId });
+
+  if (user) {
+    const newProject = new Project();
+    newProject.user = user;
+    newProject.title = requestTitle;
+    const resultNewProject = await dataSource.manager.save(newProject);
+    res.status(201).json(resultNewProject);
+  } else {
+    res.status(400).send('No such user');
+  }
+};
+
+const deleteProject = async (req: Request, res: Response) => {
+  const requestProjectId = req.params.uuid;
+
+  if (!uuidValidate(requestProjectId)) {
+    res.status(400).send('Invalid project id');
+    return;
+  }
+
+  const resultTimer = await dataSource.manager.delete(
+    Project,
+    requestProjectId
+  );
+  if (resultTimer.affected) {
+    res.status(200).send('Project deleted');
+  } else {
+    res.status(404).send('Project not found');
+  }
+};
+
+// TODO remove if don't need
 const getProject = async (req: Request, res: Response) => {
   const projectId = req.params.uuid;
 
@@ -48,4 +100,4 @@ const getProject = async (req: Request, res: Response) => {
   }
 };
 
-export { getUserProjects, getProject };
+export { getUserProjects, getProject, addUserProject, deleteProject };
